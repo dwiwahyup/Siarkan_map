7e2292ae02d78b4115d845fbee4d51540f7533a0
dwiwahyup <dwiwahyu1235@gmail.com>
2023-05-30 16:16:58 +0700

    update

---
+++ database/migrations/2023_05_30_090444_create_rules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rules', function (Blueprint $table) {
+            $table->id();
+            $table->string('jam');
+            $table->string('kepadatan');
+            $table->string('intensitas');
+            $table->string('kondisi_korban');
+            $table->string('tingkat_kerawanan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rules');
+    }
+}
